e412b3a175a023eaf291330258a3e4c1a34aa5c5
misfitestudio <humberto@misfitestudio.com>
2026-06-06 19:22:46 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'colorband-v3';
+const CACHE = 'colorband-v4';
 const ASSETS = ['/colorband/', '/colorband/index.html', '/colorband/manifest.json', '/colorband/icons/icon-192.svg', '/colorband/icons/icon-512.svg'];
 
 self.addEventListener('install', e => {
